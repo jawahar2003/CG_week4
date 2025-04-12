8adacddf5f47e86493dcdaab653b1d8cc70e3d1d
jawahar2003 <jawahar6343@gmail.com>
2025-04-12 17:30:00 +0530

    Runtime analysis

---
+++ day5/Searching/ConcatenateStrings.java
@@ -0,0 +1,20 @@
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        StringBuffer strBuffer = new StringBuffer();
+        StringBuilder strBuilder = new StringBuilder();
+        
+        long start = System.nanoTime();
+        for(int i=1; i<=1000000; i++){
+            strBuffer.append("Hello");
+        }
+        long end = System.nanoTime();
+        System.out.println("Time(in milliseconds) taken for String Buffer: " + (end - start) );
+        
+        start = System.nanoTime();
+        for(int i=1; i<=1000000; i++){
+            strBuilder.append("Hello");
+        }
+        end = System.nanoTime();
+        System.out.println("Time(in milliseconds) taken for String Builder: " + (end - start) );
+    }
+}
